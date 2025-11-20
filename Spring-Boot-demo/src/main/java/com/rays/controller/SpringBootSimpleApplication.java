55b3c1fd5bbb68ac51e4a1d83b164b97ba6bc219
VedikVishwakarma01 <vedikvishwakarma07@gmail.com>
2025-11-20 12:34:33 +0530

    done

---
+++ Spring-Boot-demo/src/main/java/com/rays/controller/SpringBootSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.rays.controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleApplication.class, args);
+		System.out.println("spring boot application starts on http://localhost:8080");
+	}
+
+}
